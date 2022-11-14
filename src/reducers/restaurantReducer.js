ae1cc71582713300d0158f7d38f996c020dd0ede
parthpandyappp <parthpandyappp@gmail.com>
2022-11-14 23:34:36 +0530

    [Chores]: Logout fixes

--- src/reducers/restaurantReducer.js
+++ src/reducers/restaurantReducer.js
@@ -1,35 +1,44 @@
 const restaurantReducer = (state, action) => {
+    console.log(action.type)
     switch (action.type) {
         case "SET_NAME":
-            return { ...state, restaurant_name: action.payload };
+return { ...state, restaurant_name: action.payload };
         case "ADD_RESTAURANT":
-            return {
-                ...state,
-                restaurants: [
-                    ...state.restaurants,
-                    { name: action.payload, created: new Date() },
-                ],
-            };
+return {
+    ...state,
+    restaurants: [
+        ...state.restaurants,
+        { name: action.payload, created: new Date() },
+    ],
+};
         case "REMOVE_RESTAURANT":
-            return {
-                ...state, restaurants: state.restaurants.filter(restaurant => restaurant.name !== action.payload)
-            };
+return {
+    ...state, restaurants: state.restaurants.filter(restaurant => restaurant.name !== action.payload)
+};
         case "ADD_BOOKMARK":
-            return {
-                ...state, restaurants: state.restaurants.filter(restaurant => restaurant.name !== action.payload.name), bookmarks: [
-                    ...state.bookmarks, action.payload
-                ]
-            };
+return {
+    ...state, restaurants: state.restaurants.filter(restaurant => restaurant.name !== action.payload.name), bookmarks: [
+        ...state.bookmarks, action.payload
+    ]
+};
         case "REMOVE_FROM_BOOKMARKS":
-            return {
-                ...state, bookmarks: state.bookmarks.filter(bookmark => bookmark.name !== action.payload)
-            }
+return {
+    ...state, bookmarks: state.bookmarks.filter(bookmark => bookmark.name !== action.payload)
+}
         case "ADD_TO_LIKES":
-            return { ...state, likes: [...state.likes, action.payload] }
+return { ...state, likes: [...state.likes, action.payload] }
         case "REMOVE_FROM_LIKES":
-            return { ...state, likes: state.likes.filter(like => like.name !== action.payload) }
+return { ...state, likes: state.likes.filter(like => like.name !== action.payload) }
+        case "BACK_TO_DEFAULT":
+console.log("LOGGEDOUT")
+return {
+    restaurant_name: "",
+    restaurants: [],
+    bookmarks: [],
+    likes: []
+}
         default:
-            return state;
+return state;
     }
 }
 
